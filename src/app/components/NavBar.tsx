27a64ebf130293e173f7b74c524230725c3add19
yashug <gosula.yaswanth2@gmail.com>
2023-05-13 15:58:50 +0530

    Navigation Bar

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,76 @@
+"use client";
+import Link from "next/link";
+import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+import { GithubIcon, LinkedInIcon, TwitterIcon } from "./Icons";
+import { motion } from "framer-motion";
+
+type CustomLinkProps = {
+  href: string;
+  title: string;
+  className?: string;
+};
+
+const CustomLink = ({ href, title, className = "" }: CustomLinkProps) => {
+  const path = usePathname();
+
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`h-[1px] inline-block absolute bg-dark -bottom-0.5 left-0 group-hover:w-full transition-[width] ease duration-300 ${
+          path === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+const NavBar = () => {
+  return (
+    <header className="w-full px-32 py-8 flex items-center justify-between">
+      <nav>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Porjects" className="mx-4" />
+        <CustomLink href="/articles" title="Articles" className="ml-4" />
+      </nav>
+      <nav className="flex justify-center items-center flex-wrap">
+        <motion.a
+          href="https://github.com/yashug"
+          target="_blank"
+          className="w-6 mx-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <GithubIcon />
+        </motion.a>
+        <motion.a
+          href="https://www.linkedin.com/in/yaswanthg5"
+          target="_blank"
+          className="w-6 mx-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <LinkedInIcon />
+        </motion.a>
+        <motion.a
+          href="https://twitter.com/urstrulyyashu5"
+          target="_blank"
+          className="w-6 ml-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <TwitterIcon />
+        </motion.a>
+      </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
-  return <main>home</main>;
+  return (
+    <main>
+      <NavBar />
+      home
+    </main>
+  );
 }
